baad0ff318168aa2a1f0f59f35106c06080fdd36
bloom510 <bloomjazzer@gmail.com>
2018-12-31 09:51:57 -0800

    Fixes menu name assignment

--- src/App.js
+++ src/App.js
@@ -19,19 +19,19 @@ class App extends Component {
   async loadModule() {
     const { state, actx } = this;   
     try {
-      await actx.audioWorklet.addModule(`worklet/${state.processor.name}.js`);
+      await actx.audioWorklet.addModule(`worklet/${state.processor.module}.js`);
       this.setState({moduleLoaded: true, status: null})
-      console.log(`loaded module ${state.processor.name}`);
+      console.log(`loaded module ${state.processor.module}`);
     } catch(e) {
       this.setState({moduleLoaded: false})
-      console.log(`Failed to load module ${state.processor.name}`);
+      console.log(`Failed to load module ${state.processor.module}`);
     }
   }
   /* handleSelect: loads modules when selected from dropdown menu.
      It also handles instantiating an AudioContext since it's likely the first user gesture.*/
   handleSelect(name, processor) {
     if(this.state.isPlaying) return;
-    this.setState({ processor: {name: processor.name, cb: processor.cb}, moduleLoaded: false, status: 'Loading module, please wait...' }, () => {
+    this.setState({ processor: {name, module: processor.name, cb: processor.cb}, moduleLoaded: false, status: 'Loading module, please wait...' }, () => {
       if(!this.actx) {
         try {
           console.log('New context instantiated')
@@ -47,10 +47,10 @@ class App extends Component {
   toggleNode(){
     const { state } = this;
     if(state.isPlaying) {
-      console.log(`stopping ${state.processor.name}`)
+      console.log(`stopping ${state.processor.module}`)
       state.node.port.postMessage(false)
     } else {
-      console.log(`playing ${state.processor.name}`)
+      console.log(`playing ${state.processor.module}`)
       const node = state.processor.cb(this);
       this.setState({ node });
       node.port.postMessage(true);          
